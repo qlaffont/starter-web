4a1a6854595ce0ed4e9f29dbb949c1e80be6addd
mike <mike@flexper.fr>
2023-08-14 15:36:50 +0200

    fix(atom): toggle working

--- components/atoms/Toggle.tsx
+++ components/atoms/Toggle.tsx
@@ -6,33 +6,35 @@ type Props = {
   id: string;
 };
 
-const checked = 'peer-checked:bg-dark-blue';
-const uncheckedColor = 'bg-dark-blue bg-opacity-60';
+const checked = 'peer-checked:bg-success';
+const uncheckedColor = 'bg-error bg-opacity-60';
 
 export const Toggle = ({ value, id, onClick }: Props) => (
-  <div className="relative h-[15px] w-[27px] cursor-pointer">
-    <label htmlFor={id}>
-      <input
-        type="checkbox"
-        id={id}
-        className="peer sr-only"
-        checked={value}
-        onChange={(e) => onClick(e.target.checked)}
-      />
+  <div className="flex w-[27px] items-center justify-center">
+    <div className="relative h-[15px] w-[27px] cursor-pointer">
+      <label htmlFor={id}>
+        <input
+          type="checkbox"
+          id={id}
+          className="peer sr-only"
+          checked={value}
+          onChange={(e) => onClick(e.target.checked)}
+        />
 
-      <span
-        className={clsx(
-          `${checked} absolute inset-0 !cursor-pointer rounded-full ${uncheckedColor} transition`,
-          value !== true ? 'opacity-60' : '',
-        )}
-      />
+        <span
+          className={clsx(
+            `${checked} absolute inset-0 !cursor-pointer rounded-full ${uncheckedColor} transition`,
+            value !== true ? 'opacity-60' : '',
+          )}
+        />
 
-      <span
-        className={clsx(
-          `absolute inset-0 m-[2px] h-[11px] w-[11px] !cursor-pointer rounded-full bg-white transition peer-checked:translate-x-[0.8rem]`,
-          value !== true ? 'opacity-60' : '',
-        )}
-      />
-    </label>
+        <span
+          className={clsx(
+            `absolute inset-0 m-[2px] h-[11px] w-[11px] !cursor-pointer rounded-full bg-white transition peer-checked:translate-x-[0.8rem]`,
+            value !== true ? 'opacity-60' : '',
+          )}
+        />
+      </label>
+    </div>
   </div>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
+import { useBoolean } from 'usehooks-ts';
+
+import { Toggle } from '../components/atoms/Toggle';
 import { EmptyLayout } from '../components/layout/EmptyLayout';
 import { useI18n } from '../i18n/useI18n';
 
 const Home = () => {
   const { t } = useI18n();
 
-  return <p>{t('pages.home.hello')}</p>;
+  const { value: isToggleOn, toggle: toggleIsToggleOn } = useBoolean(false);
+
+  return (
+    <div className="grid h-full w-full grid-cols-4">
+      <p>{t('pages.home.hello')}</p>
+
+      <Toggle value={isToggleOn} onClick={toggleIsToggleOn} id="toggle" />
+    </div>
+  );
 };
 
 Home.Layout = EmptyLayout;
