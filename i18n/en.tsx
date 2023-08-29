55d2a532478a8c142800641741831688b8a89ffd
mike <mike@flexper.fr>
2023-08-29 14:14:51 +0200

    fix(i18n): remove previous yup translations

--- i18n/en.tsx
+++ i18n/en.tsx
@@ -1,62 +1,6 @@
 import { ZodEn } from './zodEn';
 
 const lng = {
-  yup: {
-    mixed: {
-      default: 'This field is invalid',
-      required: 'This field must be completed',
-      oneOf: 'This field must contain one of these {{values}} values',
-      notOneOf: 'This field must not contain any of these {{values}} values',
-      defined: 'This field must be defined',
-    },
-    string: {
-      default: 'This field is invalid',
-      required: 'This field must be completed',
-      length: 'This field must be {{length}} characters long',
-      min: 'This field must be at least {{min}} characters long',
-      max: 'This field must have a maximum length of {{max}} characters',
-      matches: 'This field must respect the regex ({{regex}})',
-      email: 'The email address entered is invalid',
-      url: 'This field must contain a valid url (Starting with http:// or https://)',
-      uuid: 'This field must be a valid UUID',
-      trim: 'This field must be trimmed (no spaces before or after)',
-      lowercase: 'This field must be in lower case',
-      uppercase: 'This field must be capitalized',
-      phone: 'Invalid phone number',
-      alphabet: 'This field may only contain letters, hyphens or spaces',
-      digits: 'This field must contain numbers only',
-    },
-    number: {
-      min: 'This field must be greater than or equal to {{min}}',
-      max: 'This field must be smaller than or equal to {{max}}',
-      lessThan: 'This field must be smaller than {{less}}',
-      moreThan: 'This field must be smaller than {{more}}',
-      positive: 'This field must be a positive number',
-      negative: 'This field must be a negative number',
-      integer: 'This field must be an integer',
-      type: 'This field must be a number',
-    },
-    date: {
-      min: 'This field must contain a date after {{min}}',
-      max: 'This field must contain a date before {{max}}',
-      dateAfterPreviousValue: 'This field must contain a date that occurs after the previous date',
-    },
-    boolean: {
-      isValue: 'This field must have the value {{value}}',
-    },
-    object: {
-      noUnkown: 'This field contains unknown keys',
-    },
-    array: {
-      min: 'This field must contain at least {{min}} element(s)',
-      max: 'This field must contain a maximum of {{max}} element(s)',
-      length: 'This field must contain {{length}} element(s)',
-    },
-    password: {
-      length: 'Your password must be between 8 and 20 characters long',
-      notIdentical: 'Your two passwords are not identical',
-    },
-  },
   navbar: {},
   components: {
     form: {
--- i18n/fr.tsx
+++ i18n/fr.tsx
@@ -2,62 +2,6 @@ import enDict from './en';
 import { ZodFr } from './zodFr';
 
 const lng: typeof enDict = {
-  yup: {
-    mixed: {
-      default: 'Ce champ est invalide',
-      required: 'Ce champ doit être rempli',
-      oneOf: 'Ce champ doit contenir une de ces valeurs {{values}}.',
-      notOneOf: 'Ce champ ne doit pas contenir une de ces valeurs {{values}}.',
-      defined: 'Ce champ doit être défini.',
-    },
-    string: {
-      default: 'Ce champ est invalide',
-      required: 'Ce champ doit être rempli',
-      length: 'Ce champ doit faire {{length}} caractères.',
-      min: 'Ce champ doit faire au minimum {{min}} caractères.',
-      max: 'Ce champ doit faire au maximum {{max}} caractères.',
-      matches: 'Ce champ doit respecter la regex ({{regex}}).',
-      email: "L'adresse email renseignée n'est pas valide.",
-      url: 'Ce champ doit contenir une url valide. (Commençant par http:// ou https://).',
-      uuid: 'Ce champ doit être un UUID valid.',
-      trim: "Ce champ doit être trimmé (pas d'espace avant ou après).",
-      lowercase: 'Ce champ doit être en minuscule.',
-      uppercase: 'Ce champ doit être en majuscule.',
-      phone: "Le numéro de téléphone renseigné n'est pas valide",
-      alphabet: 'Ce champ ne doit comporter que des lettres, tirets, ou espaces',
-      digits: 'Ce champ ne doit comporter que des chiffres',
-    },
-    number: {
-      min: 'Ce champ doit être plus grand ou égal à {{min}}.',
-      max: 'Ce champ doit être plus petit ou égal à {{max}}.',
-      lessThan: 'Ce champ doit être plus petit que {{less}}.',
-      moreThan: 'Ce champ doit être plus petit que {{more}}.',
-      positive: 'Ce champ doit être un nombre positif.',
-      negative: 'Ce champ doit être un nombre négatif.',
-      integer: 'Ce champ doit être un nombre entier.',
-      type: 'Ce champ doit être un nombre.',
-    },
-    date: {
-      min: 'Ce champ doit contenir une date après le {{min}}.',
-      max: 'Ce champ doit contenir une date avant le {{max}}.',
-      dateAfterPreviousValue: 'Ce champ doit contenir une date qui se passe après la date précédente.',
-    },
-    boolean: {
-      isValue: 'Ce champ doit avoir la valeur {{value}}.',
-    },
-    object: {
-      noUnkown: 'Ce champ contient des clés inconnus.',
-    },
-    array: {
-      min: 'Ce champ doit contenir au minimum {{min}} élément(s).',
-      max: 'Ce champ doit contenir au maximum {{max}} élément(s).',
-      length: 'Ce champ doit contenir {{length}} élément(s).',
-    },
-    password: {
-      length: 'Votre mot de passe doit faire entre 8 et 20 caractères',
-      notIdentical: 'Vos deux mots de passe ne sont pas identiques !',
-    },
-  },
   navbar: {},
   components: {
     form: {
